8f2376a95ecba5884b4a331558345cfd820713d1
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2022-10-17 15:33:13 -0700

    Print combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,35 +7,23 @@
  */
 int main(void)
 {
-	int c, i, k, j;
+	int num1, num2;
 
-	for (c = 48; c <= 57; c++)
+	for (num1 = 0; num1 <= 98; num++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (k = 48; k <= 57; k++)
-			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
