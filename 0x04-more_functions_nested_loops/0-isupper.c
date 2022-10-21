e5ee4c6311e5f73b7e1f58ee78c237f90ea2c8e2
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2022-10-21 16:39:52 -0700

    Lower case Upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * main - check the code for Alx Students
- *
- * Return - Always 0
- */
-int main(void)
-{
-	char c;
 
-	c = "A";
-	printf("%c: %d\n", c, _isupper(c));
-	c = "a";
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: Variable text
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+
+	else
+		return (0);
+	}
 }
