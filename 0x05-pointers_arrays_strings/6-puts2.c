d0f264532f66fe66617291219f175c7be07a2bf8
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2022-10-25 18:20:50 -0700

    prints 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
 		{
-			if ((i % 2) == 0)
-				_putchar(str[i]);
-			else
-				continue;
+			_putchar(str[i]);
 		}
+		i++;
+	}
+	_putchar('\n');
 }
