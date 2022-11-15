a97e52f38f9c923a94350eedd9c4edbed615abda
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2022-11-14 20:52:16 -0800

    Dog structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,16 +13,16 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	else 
+	else
 		printf("Name: %s\n", d->name);
 
 	if (d->age < 0)
 		printf("Age: (nil)\n");
-	else 
+	else
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	else 
+	else
 		printf("Owner: %s\n", d->owner);
 }
